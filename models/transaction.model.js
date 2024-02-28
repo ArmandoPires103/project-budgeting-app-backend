22949c02ab8d6ea10897c2143d7ee4af84dae59e
armandopires <armandopires@pursuit.org>
2024-02-28 11:54:02 -0500

    Fixing mistake

---
+++ models/transaction.model.js
@@ -0,0 +1,37 @@
+module.exports = [
+    {
+      id: 1,
+      item_name: "Salary",
+      amount: 3000,
+      date: "2022-02-28",
+      from: "Employer",
+      category: "Income"
+    },
+    {
+      id: 2,
+      item_name: "Groceries",
+      amount: 50,
+      date: "2022-02-27",
+      from: "Grocery Store",
+      category: "Food"
+    },
+    {
+      id: 3,
+      item_name: "Rent",
+      amount: 1200,
+      date: "2022-02-01",
+      from: "Landlord",
+      category: "Housing"
+    },
+    {
+      id: 4,
+      item_name: "Internet Bill",
+      amount: 60,
+      date: "2022-02-15",
+      from: "Internet Provider",
+      category: "Utilities"
+    }
+  ];
+  
+
+  
